14d7d8403db864a19129894510c955ffe5cec493
Piyush Kumar <77287090+kepnok@users.noreply.github.com>
2025-07-02 20:57:18 +0530

    added restock route and added batch shcema

---
+++ src/schema/batch.schema.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+export const productBatchSchema = z.object({
+	quantity: z.number().int(),
+	expiryDate: z.preprocess(
+		(val) => val ? new Date(val as string) : undefined,
+		z.date().optional()
+	),
+	editedBy: z.enum(["manual", "ai"])
+});
+
+export type productBatchType = z.infer<typeof productBatchSchema>;
--- src/schema/products.schema.ts
+++ src/schema/products.schema.ts
@@ -8,16 +8,3 @@ export const productsSchema = z.object({
 })
 
 export type productstype = z.infer<typeof productsSchema>
-
-
-
-// id            Int          @id @default(autoincrement())
-//   sku           String       @unique 
-//   name          String
-//   quantity      Int
-//   reorderLevel  Int
-//   expiryDate    DateTime?
-//   lastRestocked DateTime
-//   warehouseId   Int
-//   warehouse     Warehouse    @relation(fields: [warehouseId], references: [id])
-//   restocks      RestockLog[]
